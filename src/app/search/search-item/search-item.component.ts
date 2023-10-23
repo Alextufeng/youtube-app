cc193e62dca688503e36056740b778359ef3026b
Alextufeng <alextufeng@yandex.by>
2023-10-23 13:05:15 +0300

    feat: eslint and base components added

---
+++ src/app/search/search-item/search-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search-item.component.html',
+  styleUrls: ['./search-item.component.scss']
+})
+export class SearchItemComponent {
+
+}
