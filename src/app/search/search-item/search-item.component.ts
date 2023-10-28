4307b008402c20e64739645188dd4c15a654786b
Alextufeng <alextufeng@yandex.by>
2023-10-29 02:21:29 +0300

    feat: result items settings added

--- src/app/search/search-item/search-item.component.ts
+++ src/app/search/search-item/search-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataItem } from '../search-item.model';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
