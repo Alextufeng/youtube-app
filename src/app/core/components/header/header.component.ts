70e48d1a5bdc76dd2e52cafb1aee0c081be33e48
Alextufeng <alextufeng@yandex.by>
2023-10-30 15:07:07 +0300

    feat: change structure

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { HeaderSearchSettingsComponent } from './components/header-search-settings/header-search-settings.component';
-import { HeaderSearchComponent } from './components/header-search/header-search.component';
-
+import { HeaderSearchComponent } from '../header-search/header-search.component';
+import { HeaderSearchSettingsComponent } from '../header-search-settings/header-search-settings.component';
 @Component({
   selector: 'app-header',
   standalone: true,
